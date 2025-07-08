c5bd4ac339c1f0123739dc7966a4083f517fd1a2
emilbowry <emil.bowry@icloud.com>
2025-07-08 08:09:25 +0100

    fixed config

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ console.log('⏳  Using this webpack.config.js');
 module.exports = {
   mode: 'development',
 
-  // 1. Point entry at your TSX bootstrap file:
+  
   entry: path.resolve(__dirname, 'src/index.tsx'),
 
   output: {
@@ -21,7 +21,7 @@ module.exports = {
     open: true,
   },
 
-  // 2. Resolve .ts/.tsx along with .js/.jsx:
+  
   resolve: {
     extensions: ['.ts', '.tsx', '.js', '.jsx'],
   },
@@ -29,49 +29,50 @@ module.exports = {
   module: {
     rules: [
       {
-        // 3. Use Babel (with @babel/preset-typescript) or ts-loader:
+        
         test: /\.[jt]sx?$/,
         exclude: /node_modules/,
         use: 'babel-loader',
       },
       {
-        test: /\.module\.css$/i,          // target only .module.css
+        test: /\.module\.css$/i,          
         use: [
           'style-loader',
      {
            loader: 'css-loader',
            options: {
-            modules: true,    // enable CSS Modules
-             esModule: false,  // export as CommonJS so default is the class map
+            modules: true,    
+             esModule: false,  
            },
          },
         ],
       },
     
-      // 2. Global CSS rule
+      
       {
-        test: /\.css$/i,                  // all other .css files
+        test: /\.css$/i,                  
         exclude: /\.module\.css$/i,
         use: [
           'style-loader',
-          'css-loader',                   // no modules option
+          'css-loader',                   
         ],
       },
       {
         test: /\.svg$/i,
-        type: 'asset/resource',   // emits a file and exports the URL
+        type: 'asset/resource',   
       },
       {
-        test: /\.(png|jpg|jpeg|gif|svg)$/i, // Matches all common image types
-        type: 'asset/resource', // This tells Webpack 5 to handle it as a file asset
-      },
-      // other asset rules...
-    ],
+        test: /\.(png|jpg|jpeg|gif|svg)$/i, 
+        type: 'asset/resource', 
+      }
+      
+    ]
   },
 
   plugins: [
     new HtmlWebpackPlugin({
       template: 'public/index.html',
     }),
-  ],
+  ]
+
 };
