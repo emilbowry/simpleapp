f476b99499dccc7b362562c8b5582159142d06db
emilbowry <emil.bowry@icloud.com>
2025-07-08 08:36:13 +0100

    reset files system

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,7 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { TitleBar } from './components/titlebar/TitleBar';
 import {Footer} from './components/footer/Footer';
-// import other features as you build them
-// import { Steps } from './features/steps/Steps';
-// import { Timeline } from './features/timeline/Timeline';
 import { FounderPage } from './pages/founderpage/FounderPage'; // <-- Import the new page
-// If you do have global styles, point at the real file:
-// import './styles/global.css';
 import globalstyles from './global.module.css';
 import {HomePage} from './HomePage'
 import { ContactPage } from './pages/contactpage/ContactPage'; 
