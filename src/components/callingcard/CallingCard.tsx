ed04594c2647a1c83eb2a757bc76bc50d07df3cb
emilbowry <emil.bowry@icloud.com>
2025-08-05 10:47:10 +0100

    clean up

--- src/components/callingcard/CallingCard.tsx
+++ src/components/callingcard/CallingCard.tsx
@@ -11,35 +11,15 @@ export interface ICallingCard {
 }
 
 
-export const createCallingCard = (
-  { Body, ratio = 0.2 }: ICallingCard
-): React.ReactElement => {
-
-  const template = `minmax(0, ${ratio * 100}%) minmax(0, 1fr)`;
-  return (
-    <section className={styles.callingCard}
-             style={{ gridTemplateColumns: template }}>
-      <Body />
-      <Logobox />
-    </section>
-  );
-};
-
 
 export class CallingCard extends React.Component<ICallingCard> {
-  // Use static defaultProps for setting default values in class components.
-  static defaultProps = {
-    ratio: 0.2,
-  };
+
 
   render() {
-    // Access `Body` and `ratio` from `this.props`.
     const { Body, ratio = 0.2  } = this.props;
 
-    // The logic for creating the template string is identical.
     const template = `minmax(0, ${ratio * 100}%) minmax(0, 1fr)`;
 
-    // The render method returns the React element directly.
     return (
       <section
         className={styles.callingCard}
@@ -51,11 +31,3 @@ export class CallingCard extends React.Component<ICallingCard> {
     );
   }
 }
-export const addCallingCard = (
-  { Body, ratio = 0.2 }: ICallingCard
-): React.ReactElement => {
-
-
-  return (<CallingCard Body={Body} ratio={ratio} />)
-  
-};
