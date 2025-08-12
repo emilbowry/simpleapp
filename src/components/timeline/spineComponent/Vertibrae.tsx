d9f7bbc031a739df843ae549e789080b29e84dcc
emilbowry <emil.bowry@icloud.com>
2025-08-12 10:07:36 +0100

    defined logo better

--- src/components/timeline/spineComponent/Vertibrae.tsx
+++ src/components/timeline/spineComponent/Vertibrae.tsx
@@ -3,7 +3,13 @@
 import React from "react";
 // import styles from "./Timeline.module.css";
 import styles from "../../../GlobalStyles.module.css";
-
+import {
+	stormy_light_green,
+	stormy_green,
+	stormy_grey,
+	stormy_blue,
+	stormy_violet,
+} from "../../../utils/defaultColours";
 import {
 	SpineComponent,
 	TSpineComponent,
@@ -24,7 +30,13 @@ export const DefaultVertibrae: React.FC<TVertibraeContent> = ({ index }) => {
 	const leftHandHalo =
 		"m -22,0 a22,22,90,0,1,22,-22l0,-2a24,24,90,0,0,-24,24m24,-22h1v-40h-2v40zm-22,22v-1h-33v2h33zm-40,0a2,2,90,1,0,8,0a2,2,90,1,0,-8,0zm2,0a2,2,90,0,1,4,0a2,2,90,0,1,-4,0z";
 
-	const colours = ["#41b2b3", "#5D7F8C", "#7F81AF", "#697085", "#7E8180"];
+	const colours = [
+		stormy_light_green,
+		stormy_blue,
+		stormy_violet,
+		stormy_grey,
+		stormy_green,
+	];
 	const colour = colours[index % 5];
 
 	return (
---
+++ src/utils/defaultColours.ts
@@ -0,0 +1,11 @@
+const colours = ["#41b2b3", "#5D7F8C", "#7F81AF", "#697085", "#7E8180"];
+
+export const stormy_light_green = "#41b2b3";
+export const stormy_green = "#7E8180";
+export const stormy_blue = "#5D7F8C";
+export const stormy_violet = "#7F81AF";
+export const stormy_grey = "#697085";
+
+export const logo_yellow = "#FFDE59";
+export const logo_blue = "#0CC0DF";
+export const title_font_colour = "#2c2c3c";
