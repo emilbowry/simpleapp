26ffef573ba09e4d65aab843b628d8542674e352
emilbowry <emil.bowry@icloud.com>
2025-09-15 23:55:25 +0100

    finalised

--- src/test_new.tsx
+++ src/test_new.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-// import { TDefinition } from "./themecomp_v2 copy";
 
 type TDefKey = `def_${string}`;
 type TDefEntries<T> = {
@@ -8,70 +7,54 @@ type TDefEntries<T> = {
 
 /* Arbitrary functions that can take arbitrary arguments, any is valid here */
 type ArbitraryArgs = any[];
-
 /* Arbitrary extensions, any is valid here */
 type ArbitraryValue = any;
 
 type TDefUpdate = TDefEntries<ArbitraryValue>;
-// type TGetEntries<T extends TDefUpdate> = (style_based: T) => T | TDefUpdate;
 type TGetEntries<T extends TDefUpdate> = (
 	style_based: T
-) => T extends IDefinition ? IDefinition : TDefUpdate;
+) => T extends TDefinition ? TDefinition : TDefUpdate;
 
-type TStaticCSS = TDefEntries<React.CSSProperties>;
-type TStylingArgs = TDefEntries<ArbitraryArgs>;
+type TDefStaticCSS = TDefEntries<React.CSSProperties>;
+type TDefStylingArgs = TDefEntries<ArbitraryArgs>;
 
-type TStyleFunction<I extends ArbitraryArgs> = <T extends TStylingArgs>(
+type TStyleFunction<I extends ArbitraryArgs> = <T extends TDefStylingArgs>(
 	default_arguments: T,
 	...args: I
 ) => React.CSSProperties;
+type TDefStylingFunction = TDefEntries<TStyleFunction<ArbitraryArgs>>;
 
-type TCSSApplicator<TStyleArgs extends React.CSSProperties[] | IDefinition[]> =
+type TProtoStyle = Pick<TDefStaticCSS, "def_default_static_css"> &
+	Pick<TDefStylingArgs, "def_default_args"> &
+	Pick<TDefStylingFunction, "def_default_styling_function">;
+type TDefinition = TDefUpdate & TProtoStyle;
+
+type TCSSApplicator<TStyleArgs extends React.CSSProperties[] | TDefinition[]> =
 	(...args: [...TStyleArgs, ...ArbitraryArgs]) => void;
 
-type TStylingFunction = TDefEntries<TStyleFunction<ArbitraryArgs>>;
-
-type TProtoStyle = Pick<TStaticCSS, "def_default_static_css"> &
-	Pick<TStylingArgs, "def_default_args"> &
-	Pick<TStylingFunction, "def_default_styling_function">;
-// type IDefinition = TDefUpdate & TProtoStyle; // cant remember why chose interface
-interface IDefinition extends TDefEntries<ArbitraryValue>, TProtoStyle {}
-
 type TProcessor<T> = (arg: T) => void;
-type TProcessors<T extends IDefinition> = {
+type TProcessors<T extends TDefinition> = {
 	[K in keyof T]: TProcessor<T[K]>;
 };
 
-/*
-
-Sanity check
-
-
-type a = TCSSApplicator<
-	[React.CSSProperties]
-> extends TProcessor<React.CSSProperties>
-	? true
-	: false;
-
-Returns True
-
-*/
 interface BaseStyle {
 	processors: TProcessors<TProtoStyle>;
 	definitions: TProtoStyle;
 }
 
-export interface IStyle<TDefs extends IDefinition>
+interface IStyle<TDefs extends TDefinition>
 	extends React.CSSProperties,
 		BaseStyle {
+	buildFunctor: (CSS: React.CSSProperties) => TStyleFunctor<TProtoStyle>;
+	extractCSS: (this: TStyleFunctor<any>) => React.CSSProperties;
 	getDefinition: () => ReturnType<TGetEntries<TDefs>>;
 
 	definitions: TDefs;
 	processors: TProcessors<TDefs>;
 
 	/* Construct Time procedure */
-	constructor: Function;
-	construction: {
+	constructor: TStyleFunctor<TDefs> | Function;
+	initialisation: {
 		computer: <T extends TDefs>(proto_style: T) => void;
 
 		resolver: TCSSApplicator<[TDefUpdate]>;
@@ -79,10 +62,10 @@ export interface IStyle<TDefs extends IDefinition>
 
 	/* Call Time procedure */
 	functor: TStyleFunctor<TDefs>;
-	invocation: {
+	mapping: {
 		computer: (...args: ArbitraryArgs) => React.CSSProperties;
 
-		resolver: TCSSApplicator<[TStyleFunctor<TDefs>, React.CSSProperties]>;
+		resolver: TCSSApplicator<[React.CSSProperties]>;
 	};
 }
 
@@ -97,12 +80,13 @@ const getUpdate = <T extends TDefUpdate>(
 	});
 	return def as ReturnType<TGetEntries<T>>;
 };
-interface IFunctor<in out I extends IDefinition> {
+
+interface IFunctor<in out I extends TDefinition> {
 	(...args: ArbitraryArgs): TStyleFunctor<I>;
 	new <O extends I>(...args: ArbitraryArgs): TStyleFunctor<O>;
 }
 
-type TStyleFunctor<T extends IDefinition> = Style<T> &
+type TStyleFunctor<T extends TDefinition> = Style<T> &
 	IFunctor<T> &
 	React.CSSProperties;
 
@@ -110,7 +94,7 @@ class ProtoStyle implements BaseStyle {
 	definitions: TProtoStyle = {
 		def_default_static_css: {},
 		def_default_styling_function: (...args) => ({}),
-		def_default_args: [,], // i dont know why this prevents err in process.default_args
+		def_default_args: [],
 	};
 
 	processors: TProcessors<TProtoStyle> = {
@@ -124,28 +108,12 @@ class Style<TDefs extends Required<TProtoStyle>>
 	extends ProtoStyle
 	implements IStyle<TDefs>
 {
-	/* 
-	This is still statically type checkable! (from some initial testing)
-	any is the cannonical way to access the upper class, additionally
-
-	- we cant inspect what our superclass has as its generic extension
-	- super class may have a different restriction, not TDefs
-	- TProtoStyle is too restrictive
-	- IDefinition is too broad to be checked
-	
-	- (this as any).definitions preserves static checking: LHS RHS comparison
-
-	- ergo any
-	*/
 	definitions: TDefs = {
-		...(this as any).definitions,
+		...(this as typeof this).definitions,
 	};
 
-	/*
-	Same as above
-	*/
 	processors: TProcessors<TDefs> = {
-		...(this as any).processors,
+		...(this as typeof this).processors,
 		...{
 			def_default_static_css: (static_style) => {
 				this.definitions.def_default_static_css = static_style;
@@ -161,47 +129,56 @@ class Style<TDefs extends Required<TProtoStyle>>
 			},
 		},
 	};
+	extractCSS() {
+		const exclusionList = Object.getOwnPropertyNames(
+			new (this.constructor as typeof this.functor)()
+		);
+
+		const css = {} as React.CSSProperties;
+		for (const key in this) {
+			if (!exclusionList.includes(key)) {
+				Object.assign(css, { [key]: this[key] });
+			}
+		}
+		return css;
+	}
+	buildFunctor(css: React.CSSProperties) {
+		return new Style<TProtoStyle>({
+			def_default_static_css: css,
+		}) as TStyleFunctor<TProtoStyle>;
+	}
+	functor: TStyleFunctor<TDefs>;
 	constructor(proto_style: TDefUpdate = {}) {
 		super();
-
-		/* 
-		Maybe Definitions should be static
-
-		// this.definitions = {
-		// 	...(super.constructor as typeof Style).definitions,
-		// 	...(this.constructor as typeof Style).definitions,
-		// } as T;
-		*/
-		this.construction.computer({ ...this.definitions, ...proto_style });
-		this.construction.resolver(proto_style);
-
+		this.initialisation.computer({
+			...this.definitions,
+			...proto_style,
+		});
 		let self = this;
-		const functor = function (...args: ArbitraryArgs) {
+
+		this.functor = function functor(...args: ArbitraryArgs) {
 			if (new.target) {
-				const def = self.getDefinition();
+				const def = self.functor.getDefinition();
 				return new Style(def);
 			}
-			// self.functor(functor, ...args);
-			return self.functor(functor, ...args);
-		} as TStyleFunctor<TDefs>;
-		Object.assign(functor, { ...this });
-		Object.setPrototypeOf(functor, Style.prototype);
-		functor.functor = functor;
-		return functor;
-	}
-	functor = ((self: TStyleFunctor<TDefs>, ...args: ArbitraryArgs) => {
-		const computed_style = self.invocation.computer(...args);
-		self.invocation.resolver(self, computed_style);
-		return self;
-	}) as TStyleFunctor<TDefs>;
-	// necessary casting since cannonically arrow functions cant be newable, but this can
+			const computed_style = self.functor.mapping.computer(...args);
 
-	construction = {
+			self.functor.mapping.resolver(computed_style);
+			return self.functor;
+		} as TStyleFunctor<TDefs>;
+
+		Object.assign(this.functor, { ...this });
+		this.functor.initialisation.resolver(proto_style);
+		Object.setPrototypeOf(this.functor, this);
+		return this.functor;
+	}
+
+	initialisation = {
 		computer: <T extends TDefs>(proto_style: T): void => {
 			const definitionsToProcess = getUpdate(proto_style);
 
 			for (const key in definitionsToProcess) {
-				const defKey = key as keyof IDefinition;
+				const defKey = key as keyof TDefinition;
 
 				if (defKey in this.processors) {
 					const processor =
@@ -218,23 +195,19 @@ class Style<TDefs extends Required<TProtoStyle>>
 			[TDefUpdate]
 		>,
 	};
-	invocation = {
+	mapping = {
 		computer: (...args: ArbitraryArgs) => {
 			return this.definitions.def_default_styling_function(
 				{ def_default_args: this.definitions.def_default_args }, //obey interface correctly
 				...args
 			);
 		},
-		resolver: ((self, style_based, ...args) => {
-			Object.assign(self, { ...style_based });
-		}) as TCSSApplicator<[TStyleFunctor<TDefs>, React.CSSProperties]>,
+		resolver: ((style_based, ...args) => {
+			Object.assign(this.functor, { ...style_based });
+		}) as TCSSApplicator<[React.CSSProperties]>,
 	};
 
 	getDefinition() {
-		/*
-		if input of `getUpdate` is complete, so is output
-		 so this is correct
-	*/
 		return getUpdate(this.definitions);
 	}
 }
@@ -261,11 +234,31 @@ const s = new Style<TProtoStyle>(proto_style) as TStyleFunctor<TProtoStyle>;
 
 console.log(s);
 console.log(s("green"));
+
+console.log(s.extractCSS());
 console.log(s);
 console.log(s());
-console.log(s === s());
-// /* Dont even have to annotate it after the first construction */
+console.log(s === s("green"));
+/* Dont even have to annotate it after the first construction */
 const s2 = new s();
-console.log(s2());
-console.log(s2 !== s);
+console.log(s2);
+console.log(s2 instanceof s);
 console.log(new s2());
+console.log();
+
+/* 
+		SO CLOSE BUT new.target! doesnt exist :(
+		EVEN WORKS AS AN ARROW!
+
+		// functor = ((args:TStyleFunctor<TDefs>, ...args: ArbitraryArgs) => {
+		// 	if (new.target) {
+		// 		const def = this.getDefinition();
+		// 		return new Style(def);
+		// 	}
+
+		// 	const computed_style = this.functor.mapping.computer(...args);
+		// 	this.mapping.resolver(computed_style);
+		// 	return this as any;
+		// } as TStyleFunctor<TDefs>;
+		// necessary casting since cannonically arrow functions cant be newable, but this can
+	*/
