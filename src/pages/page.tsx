c361983a3e9bb006ce8ce4dce7a7ea32d6578f9c
emilbowry <emil.bowry@icloud.com>
2025-10-10 16:36:25 +0100

    changed page to fc

--- src/pages/page.tsx
+++ src/pages/page.tsx
@@ -14,53 +14,49 @@ const pageStyle: React.CSSProperties = {
 	flexDirection: "column", // Have to do both
 };
 
-export class Page extends React.Component<{
+export const Page: React.FC<{
 	page: React.FC;
 	bg?: boolean;
 	useCursor?: boolean;
-}> {
-	render() {
-		const { page: Page, bg = false, useCursor = true } = this.props;
+}> = ({ page: Page, bg = false, useCursor = true }) => {
+	return (
+		<>
+			{bg ? <div style={BackgroundStyle}></div> : null}
+			{useCursor ? <CustomCursor /> : null}
 
-		return (
-			<>
-				{bg ? <div style={BackgroundStyle}></div> : null}
-				{useCursor ? <CustomCursor /> : null}
-
-				<main
-					key={location.pathname}
+			<main
+				key={location.pathname}
+				style={{
+					position: "relative",
+					height: "100%",
+					maxWidth: "100%",
+					overflow: "clip",
+					display: "flex",
+					flexDirection: "column",
+					zIndex: "5",
+				}}
+			>
+				<AppTitleBar />
+				<div
+					style={
+						{
+							// height: "5vh",
+						}
+					}
+				/>
+				<section
+					className="aos-ignore"
 					style={{
-						position: "relative",
-						height: "100%",
-						maxWidth: "100%",
-						overflow: "clip",
-						display: "flex",
-						flexDirection: "column",
-						zIndex: "5",
+						position: "absolute",
+						...pageStyle,
+						marginTop: `${VISIBLE_TITLEBAR_HEIGHT}vh`,
 					}}
 				>
-					<AppTitleBar />
-					<div
-						style={
-							{
-								// height: "5vh",
-							}
-						}
-					/>
-					<section
-						className="aos-ignore"
-						style={{
-							position: "absolute",
-							...pageStyle,
-							marginTop: `${VISIBLE_TITLEBAR_HEIGHT}vh`,
-						}}
-					>
-						<Page />
-					</section>
-				</main>
+					<Page />
+				</section>
+			</main>
 
-				{bg ? Footer : null}
-			</>
-		);
-	}
-}
+			{bg ? Footer : null}
+		</>
+	);
+};
