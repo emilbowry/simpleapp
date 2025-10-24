40a787c4db869e68a1f6d0f8706a27fd3033c29a
emilbowry <emil.bowry@icloud.com>
2025-10-24 22:30:56 +0100

    try webpack again

--- webpack.config.js
+++ webpack.config.js
@@ -1,86 +1,3 @@
-// const path = require("path");
-// const HtmlWebpackPlugin = require("html-webpack-plugin");
-
-// module.exports = (env, argv) => {
-// 	const isProduction = argv.mode === "production";
-
-// 	return {
-// 		mode: isProduction ? "production" : "development",
-
-// 		entry: path.resolve(__dirname, "src/index.tsx"),
-
-// 		output: {
-// 			path: path.resolve(__dirname, "dist"),
-// 			filename: "bundle.js",
-// 			clean: true,
-// 		},
-
-// 		devtool: isProduction ? "source-map" : "eval-source-map",
-
-// 		devServer: isProduction
-// 			? undefined
-// 			: {
-// 					static: path.resolve(__dirname, "dist"),
-// 					port: 3000,
-// 					hot: true,
-// 					open: true,
-// 			  },
-
-// 		resolve: {
-// 			extensions: [".ts", ".tsx", ".js", ".jsx"],
-// 		},
-
-// 		module: {
-// 			rules: [
-// 				{
-// 					test: /\.[jt]sx?$/,
-// 					exclude: /node_modules/,
-// 					use: "babel-loader",
-// 				},
-// 				{
-// 					test: /\.module\.css$/i,
-// 					use: [
-// 						"style-loader",
-// 						{
-// 							loader: "css-loader",
-// 							options: {
-// 								modules: true,
-// 								esModule: false,
-// 							},
-// 						},
-// 					],
-// 				},
-// 				{
-// 					test: /\.(png|jpe?g|gif)$/i,
-// 					oneOf: [
-// 						{
-// 							resourceQuery: /inline/,
-// 							type: "asset/inline",
-// 						},
-// 						{
-// 							type: "asset/resource",
-// 						},
-// 					],
-// 				},
-// 				{
-// 					test: /\.css$/i,
-// 					exclude: /\.module\.css$/i,
-// 					use: ["style-loader", "css-loader"],
-// 				},
-// 				{
-// 					test: /\.svg$/i,
-// 					type: "asset/resource",
-// 				},
-// 			],
-// 		},
-
-// 		plugins: [
-// 			new HtmlWebpackPlugin({
-// 				template: "public/index.html",
-// 			}),
-// 		],
-// 	};
-// };
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
@@ -134,15 +51,14 @@ module.exports = (env, argv) => {
 					],
 				},
 				{
-					test: /\.(ico|json|png)$/i,
-					include: [path.resolve(__dirname, "public")],
+					test: /favicon|apple-touch-icon|android-chrome|manifest/i,
 					type: "asset/resource",
 					generator: {
 						filename: "[name][ext]",
 					},
 				},
 				{
-					test: /\.(jpe?g|gif)$/i,
+					test: /\.(png|jpe?g|gif)$/i,
 					oneOf: [
 						{
 							resourceQuery: /inline/,
